5ed79540b40a4e7f8aea2d40261835521506d195
João Paulo <joaopauloalencarrodrigues@gmail.com>
2025-03-25 10:58:45 -0300

    fix: re-adds toggleterm plugin file ~

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -17,14 +17,15 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
-      size = 20,
+      size = 19,
       open_mapping = [[<c-\>]],
       hide_numbers = false,
       shade_filetypes = {},
       shade_terminals = true,
-      shading_factor = 2,
+      shading_factor = 1,
       start_in_insert = true,
       insert_mappings = true,
+      terminal_mappings = true,
       persist_size = true,
       direction = "float",
       close_on_exit = true,
@@ -37,6 +38,21 @@ return {
           background = "Normal",
         },
       },
+      config = function()
+        function _G.set_terminal_keymaps()
+          local opts = { noremap = true }
+          vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+          vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        end
+
+        -- Apply the key mappings only for toggleterm windows
+        -- vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+      end,
     }
   },
 
